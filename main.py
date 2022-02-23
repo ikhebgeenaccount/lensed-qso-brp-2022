fe9376283ca8feae945c55b3b19c0c12405909aa
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-02-23 12:30:10 +0100

    fixed code for loglog

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from src.lensed_qso import LensedQSO
 
 if __name__ == '__main__':
-	lqso = LensedQSO('B1152+200')
-	lqso.plot_spectrum()
-
-	plt.show()
+    lqso = LensedQSO('B1152+200')
+    lqso.plot_spectrum()
+    lqso.plot_spectrum_loglog()
+    plt.show()
