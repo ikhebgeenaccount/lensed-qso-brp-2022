21f03590ba4dc6d3a36b9dcb65d5a45836102d14
unknown <lars.abbo@gmail.com>
2022-02-23 16:41:07 +0100

    Added filtering for plot

--- src/lensed_qso.py
+++ src/lensed_qso.py
@@ -27,7 +27,12 @@ class LensedQSO:
         # For every unique source, add their data separately
         for l in self.sed.source.unique():
             # Filter based on source
-            sel = self.sed[self.sed.source == l]
+            # Only take those that have both a wavelength and a total flux
+            sel = self.sed[(self.sed.source == l) * (self.sed.wavelength > 0) * (self.sed.flux_total > 0)]
+
+            # If there are no entries after filtering, continue to next source
+            if len(sel) == 0:
+                continue
 
             # Separate upper limits from regular data points
             sel_upper_limit = sel[sel.upper_limit == 1]
@@ -35,15 +40,18 @@ class LensedQSO:
 
             # Plot regular data points and upper limits separately, upper limits with special marker
             le_1, _, _ = ax.errorbar(sel_reg.wavelength, sel_reg.flux_total, sel_reg.flux_err, fmt='o', label=l, **kwargs)
-            le_2, _, _ = ax.errorbar(sel_upper_limit.wavelength, sel_upper_limit.flux_total, sel_upper_limit.flux_err,
-                                     fmt='o', label=l, marker='v', color=le_1.get_color(), **kwargs)
 
-            legend_list.append((le_1, le_2))
+            if len(sel_upper_limit) > 0:
+                le_2, _, _ = ax.errorbar(sel_upper_limit.wavelength, sel_upper_limit.flux_total, sel_upper_limit.flux_err,
+                                         fmt='o', label=l, marker='v', color=le_1.get_color(), **kwargs)
+                legend_list.append((le_1, le_2))
+            else:
+                legend_list.append(le_1)
 
             # upper_limits += (le_2, )
 
         # ax.legend(legend_list + [upper_limits], list(self.sed.source.unique()) + ['upper limit'], loc='upper left', handler_map={tuple: HandlerTuple(ndivide=None)})
-        ax.legend(legend_list, self.sed.source.unique(), loc='upper left', handler_map={tuple: HandlerTuple(ndivide=None)})
+        ax.legend(legend_list, self.sed[(self.sed.wavelength > 0) * (self.sed.flux_total > 0)].source.unique(), loc='upper left', handler_map={tuple: HandlerTuple(ndivide=None)})
         ax.set_xscale('log')
 
         if loglog:
