268306d1e7cdd87ca62a4e701095513e98e69776
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-02-23 12:11:44 +0100

    made some changes to sed.csv for it to run in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from src.lensed_qso import LensedQSO
 
 if __name__ == '__main__':
-	lqso = LensedQSO('J0806+2006')
+	lqso = LensedQSO('B1152+200')
 	lqso.plot_spectrum()
 
 	plt.show()
