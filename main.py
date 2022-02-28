fd51e82a0cc3fa5ce0df129b52681bb684edd567
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-02-28 09:53:22 +0100

    Sed for sdss and panstarrs

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
 from src.lensed_qso import LensedQSO
+from src.mags_to_fluxes import mags_to_fluxes
 
 if __name__ == '__main__':
-    lqso = LensedQSO('B1152+200')
+    lqso = LensedQSO('J1455+1447')
     lqso.plot_spectrum()
     lqso.plot_spectrum(loglog=True)
+    mags_to_fluxes('J1455+1447')
     plt.show()
