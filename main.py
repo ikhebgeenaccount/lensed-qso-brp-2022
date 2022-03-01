9e361fa86729e79ccfa569a387aeff02d91282d3
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-01 10:06:57 +0100

    Ran code for 1455

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from src.lensed_qso import LensedQSO
 from src.mags_to_fluxes import mags_to_fluxes
 
 if __name__ == '__main__':
-    mags_to_fluxes('B1152+200')
-    lqso = LensedQSO('B1152+200')
+    mags_to_fluxes('J1524+4409')
+    lqso = LensedQSO('J1524+4409')
     lqso.plot_spectrum()
     lqso.plot_spectrum(loglog=True)
     
