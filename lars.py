2c396f6891da2b74b3fee44c7dcbe7aac0e7f431
ikhebgeenaccount <lars.abbo@gmail.com>
2022-03-01 13:46:53 +0100

    Added calculating flux ratios and diffs between SDSS and PanSTARRS data, added COSMOGRAIL paper for J1650+4251

--- lars.py
+++ lars.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 from src.lensed_qso import LensedQSO
 from src.mags_to_fluxes import mags_to_fluxes
 
 GALAXIES = ['B1152+200', 'B1600+434', 'B1608+656', 'J0806+2006', 'J0924+0219', 'J1330+1810', 'J1455+1447', 'J1524+4409', 'J1633+3134', 'J1650+4251']
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
+    flux_discrepancy = pd.DataFrame(columns=['galaxy', 'filter', 'sdss', 'panstarrs', 'ratio', 'diff'])
+    
+    ratios = []
+    diffs = []
+    
     for g in GALAXIES:
         try:
             mags_to_fluxes(g)
@@ -14,11 +21,31 @@ if __name__ == '__main__':
 
         print(g)
         # TODO: flux ratios and difference between same SDSS and PanSTARRS bands
-
+        
         lqso = LensedQSO(g)
         # lqso.plot_spectrum()
         lqso.plot_spectrum(loglog=True)
         lqso.plot_spectrum(mags=True)
+        
+        for i, r in lqso.sed.iterrows():
+            if r['filter'] in ['g', 'r', 'i', 'z']:
+                source = 'sdss' if 'SDSS' in r.source else 'panstarrs'
+                flux_discrepancy.loc[r['filter'], source] = r.flux_total
+        
+        flux_discrepancy['ratio'] = flux_discrepancy.sdss.div(flux_discrepancy.panstarrs.where(flux_discrepancy.panstarrs != 0, np.nan))
+        flux_discrepancy['diff'] = flux_discrepancy['sdss'] - flux_discrepancy['panstarrs']
+        print(flux_discrepancy)
+        print()
+        
+        ratios.append(list(flux_discrepancy['ratio']))
+        diffs.append(list(flux_discrepancy['diff']))
+    
+    ratios = np.array(ratios).flatten()
+    diffs= np.array(diffs).flatten()
+    
+    print(f'ratio avg: {np.nanmean(ratios)}, ratio std: {np.nanstd(ratios)}')
+    print(f'diff avg: {np.nanmean(diffs)}, diff std: {np.nanstd(diffs)}')
+            
 
     # lqso.filtered_sed['error %'] = lqso.filtered_sed.flux_err / lqso.filtered_sed.flux_total
     #
