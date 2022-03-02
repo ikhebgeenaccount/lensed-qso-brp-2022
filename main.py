7ee4800f9707c2ebb1132e01d2247f6356d4b9a7
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-02 09:59:01 +0100

    lol

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from src.lensed_qso import LensedQSO
 from src.mags_to_fluxes import mags_to_fluxes
 
 if __name__ == '__main__':
+<<<<<<< Updated upstream
     gal_name = 'J0806+2006'
+=======
+    gal_name='J0806+2006'
+>>>>>>> Stashed changes
     mags_to_fluxes(gal_name)
     lqso = LensedQSO(gal_name)
     lqso.plot_spectrum()
