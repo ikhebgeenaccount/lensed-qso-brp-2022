07cde691ef5c165a928c12b4ec0740a79340db99
ikhebgeenaccount <lars.abbo@gmail.com>
2022-03-02 12:05:24 +0100

    Added filtering of psf and ap magnitudes for panstarrs

--- lars.py
+++ lars.py
@@ -5,7 +5,10 @@ import pandas as pd
 from src.lensed_qso import LensedQSO
 from src.mags_to_fluxes import mags_to_fluxes
 
+# ratio avg: 1.4543629655671957, ratio std: 0.37071640199561534
+
 GALAXIES = ['B1152+200', 'B1600+434', 'B1608+656', 'J0806+2006', 'J0924+0219', 'J1330+1810', 'J1455+1447', 'J1524+4409', 'J1633+3134', 'J1650+4251']
+SDSS_V_PANSTARRS_GS = ['B1152+200', 'B1600+434', 'J0806+2006', 'J1633+3134', 'J1455+1447', 'J1524+4409']
 
 if __name__ == '__main__':    
     flux_discrepancy = pd.DataFrame(columns=['galaxy', 'filter', 'sdss', 'panstarrs', 'ratio', 'diff'])
@@ -13,13 +16,15 @@ if __name__ == '__main__':
     ratios = []
     diffs = []
     
-    for g in GALAXIES:
+    panstarrs_mag = 'ap'
+    
+    for g in SDSS_V_PANSTARRS_GS:
         try:
             mags_to_fluxes(g)
         except FileNotFoundError:
             continue
 
-        print(g)
+        print(f'{g}, PanSTARRS mag choice: {panstarrs_mag}')
         # TODO: flux ratios and difference between same SDSS and PanSTARRS bands
         
         lqso = LensedQSO(g)
@@ -28,11 +33,15 @@ if __name__ == '__main__':
         lqso.plot_spectrum(mags=True)
         
         for i, r in lqso.sed.iterrows():
+            if 'SDSS' not in r['source'] and panstarrs_mag not in r['source']:
+                continue
             if r['filter'] in ['g', 'r', 'i', 'z']:
                 source = 'sdss' if 'SDSS' in r.source else 'panstarrs'
                 flux_discrepancy.loc[r['filter'], source] = r.flux_total
         
         flux_discrepancy['ratio'] = flux_discrepancy.sdss.div(flux_discrepancy.panstarrs.where(flux_discrepancy.panstarrs != 0, np.nan))
+        # Make all ratios > 1 so we can compare
+        flux_discrepancy['ratio'] = flux_discrepancy['ratio'].apply(lambda r: 1. / r if r < 1  else r)
         flux_discrepancy['diff'] = flux_discrepancy['sdss'] - flux_discrepancy['panstarrs']
         print(flux_discrepancy)
         print()
