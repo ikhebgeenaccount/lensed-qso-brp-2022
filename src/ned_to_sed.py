64ecd561b15362c2d9a08f1f0f39c85eee96e70a
ikhebgeenaccount <lars.abbo@gmail.com>
2022-03-04 09:48:30 +0100

    Added GALEX default source

--- src/ned_to_sed.py
+++ src/ned_to_sed.py
@@ -14,19 +14,19 @@ def ned_table_to_sed(lqso, ned_file='ned.txt', wavelength_conversion=1e4, flux_c
     :param wavelength_conversion: default: um to Angstrom
     :param flux_conversion: default: Jy to mJy
     :return:
-    """    
+    """
     ned_df = pd.read_csv(os.path.join('data', lqso.name, ned_file), delimiter='|')
-    
+
     # Filter out non-measurement
     fs = np.sum(ned_df['Flux Density'].isna())
     if fs > 0:
         warnings.warn('Filtered out ' + str(fs) + ' measurements, check if this is correct!')
-        ned_df = ned_df[ned_df['Flux Density'].notna()]    
-    
+        ned_df = ned_df[ned_df['Flux Density'].notna()]
+
     if not np.all(np.where(ned_df['Upper limit of uncertainty'] == ned_df['Lower limit of uncertainty'], 1, 0)):
         # print(ned_df[['Upper limit of uncertainty', 'Lower limit of uncertainty']])
         raise NotImplementedError('Upper and lower limit of uncertainty are different, this cannot be handled.')
-        
+
     if np.all(np.where(ned_df['Upper limit of Flux Density'] == np.nan, 1, 0)) or np.all(np.where(ned_df['Lower limit of Flux Density'] == np.nan, 1, 0)):
         raise NotImplementedError('Table has upper or lower limit for flux density, this cannot be handled.')
 
@@ -34,22 +34,22 @@ def ned_table_to_sed(lqso, ned_file='ned.txt', wavelength_conversion=1e4, flux_c
     ned_df['wavelength'] = ned_df['Wavelength'] * wavelength_conversion
     ned_df['flux_total'] = ned_df['Flux Density'] * flux_conversion
     ned_df['flux_err'] = ned_df['Upper limit of uncertainty'] * flux_conversion
-    
+
     ned_df['observed_passband'] = ned_df['Observed Passband'].apply(lambda v: v.strip())
     ned_df['Qualifiers'] = ned_df['Qualifiers'].apply(lambda v: v.strip())
 
     wls = ned_df['wavelength'].unique()
-    
+
     qualis = ned_df['Qualifiers'].unique()
-    
+
     # Check if qualifier exists and exists in qualis
     if qualifier is None:
         qi = input(f'Found qualifiers: {qualis}, input index of which one to use (zero-indexed):')
     elif qualifier not in qualis:
         qi = input(f'Given qualifier {qualifier} not found in qualifiers {qualis}, input index of which one to use (zero-indexed):')
-        
+
     qualifier = qualis[int(qi)]
-        
+
     if qualifier not in qualis:
         raise ValueError('Qualifier ' + qualifier + ' not found in qualifiers of given table.')
 
@@ -65,7 +65,7 @@ def ned_table_to_sed(lqso, ned_file='ned.txt', wavelength_conversion=1e4, flux_c
         if sel.shape[0] > 1:
             warnings.warn('Multiple values found for wavelength ' + str(wl) +', qualifier ' + qualifier)
             continue
-        
+
         if sel.shape[0] == 0:
             warnings.warn('No values found for wavelength ' + str(wl) +', qualifier ' + qualifier)
             continue
@@ -74,13 +74,15 @@ def ned_table_to_sed(lqso, ned_file='ned.txt', wavelength_conversion=1e4, flux_c
         flux_total = sel['flux_total'].values[0]
         flux_err = sel['flux_err'].values[0]
         observed_passband = sel['observed_passband'].values[0]
-        
+
         source = ''
         # Check some default values for source
         if 'WISE' in observed_passband:
             source = 'WISE'
         elif 'Chandra' in observed_passband:
             source = 'Chandra'
+        elif 'GALEX' in observed_passband:
+            source = 'GALEX'
 
         # Check if entry is not in lqso.sed yet
         if lqso.sed[(lqso.sed['wavelength'] == wl) & (lqso.sed['observed_passband'] == observed_passband)].empty:
