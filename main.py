0912a88d056e9ac9fb31869f26f23c3f0b37f540
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-04 16:35:52 +0100

    I did not change this?

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     galaxy = 'B1600+434'
     lqso = LensedQSO(galaxy)
     
-   # mags_to_fluxes(lqso)
+    mags_to_fluxes(lqso)
     #ned_table_to_sed(lqso,'NED_WISE', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
     lqso.plot_spectrum(loglog=True)
     
