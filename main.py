8a19fc76278faa52c9ca17952b39ab3608245711
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-04 09:37:33 +0100

    ran code

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     galaxy = 'J0924+0219'
     lqso = LensedQSO(galaxy)
     
-    mags_to_fluxes(lqso)
+   # mags_to_fluxes(lqso)
     ned_table_to_sed(lqso,'NED_12')
     lqso.plot_spectrum(loglog=True)
     
