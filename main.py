8c310a401406b0bf02f11758888ba93545e40c8f
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-07 15:18:13 +0100

    lot done

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from src.mags_to_fluxes import mags_to_fluxes
 from src.ned_to_sed import ned_table_to_sed
 
 if __name__ == '__main__':
-    galaxy = 'J0806+2006'
+    galaxy = 'J1330+1810'
     lqso = LensedQSO(galaxy)
     
-    #mags_to_fluxes(lqso)
-   # ned_table_to_sed(lqso,'ned_2mass', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
+    mags_to_fluxes(lqso)
+    ned_table_to_sed(lqso,'ned_galex', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
     lqso.plot_spectrum(loglog=True)
     
     plt.show()
