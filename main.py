d116269b3807efc5f32946bbfd6e26697fa13ca0
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-09 09:32:58 +0100

    ran code or gemini K

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from src.ned_to_sed import ned_table_to_sed
 from src.xml_to_txt import xml_to_txt
 
 if __name__ == '__main__':
-    xml_to_txt('Gemini_K.xml')
+    xml_to_txt('Gemini_K.xml', 'Gemini_K.txt')
+    
     #galaxy = 'J0924+0219'
     #lqso = LensedQSO(galaxy)
-    
     #mags_to_fluxes(lqso)
     #ned_table_to_sed(lqso,'ned_galex', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
     #lqso.plot_spectrum(loglog=True)
--- src/xml_to_txt.py
+++ src/xml_to_txt.py
@@ -6,22 +6,40 @@ import os
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 
-def xml_to_txt(filename):
+def xml_to_txt(xmlname,txtname):
     """
-    The xml files taken from he filter profile service website follow a certain
-    structure, this is printed for all the datapoints (wavelength, transmission)
+    The xml files taken from he filter profile service website follow the 
+    same structure, this fil reads the entries in from the xmlname file and 
+    writes them to the txtname file, assuming both the files are in the 
+    FIlterprofiles folder. 
     """
-    tree = ET.parse(os.path.join('data','Filterprofiles',filename))
+    #Reading in the xml file
+    tree = ET.parse(os.path.join('data','Filterprofiles',xmlname))
     root = tree.getroot()
-    transmission=[]
-    wavelength=[]
+    
+    #For plotting the filterprofile
+    transmission = []
+    wavelength = []
+    
+    #For writing to the new file
+    save_string = ''
+    
+    #Seperate into wavelength and transmission
     for TD in root.iter("TD"):
-        if float(TD.text) < 1 :
+        if float(TD.text) <= 1 :
             transmission.append(float(TD.text))
+            save_string += f' {TD.text}\n'
         else:
             wavelength.append(float(TD.text))
-            
-    print(wavelength)
-    print(transmission)
-    plt.plot(wavelength,transmission)
+            save_string += f'{TD.text}'
+    #plotting to check everything
+    plt.plot(wavelength, transmission)
+    
+    #writing to the new file
+    file = open(os.path.join('data','Filterprofiles',txtname), "w")
+    file.write(save_string)
+    file.close()
+    
+    
+
             
