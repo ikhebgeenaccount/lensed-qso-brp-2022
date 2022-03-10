523f3a25669a047e6f392c37dfc9e86217fb334a
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-10 11:36:33 +0100

    wrote tophat

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from src.lensed_qso import LensedQSO
 from src.mags_to_fluxes import mags_to_fluxes
 from src.ned_to_sed import ned_table_to_sed
 from src.xml_to_txt import xml_to_txt
+from src.tophat import tophat
 
 if __name__ == '__main__':
-    xml_to_txt('HST.ACS_HRC.F330W.xml', 'HST.ACS_HRC.F330W.txt')
+    #xml_to_txt('HST.ACS_HRC.F330W.xml', 'HST.ACS_HRC.F330W.txt')
     
-    #galaxy = 'J1455+1447'
-    #lqso = LensedQSO(galaxy)
+    galaxy = 'J1455+1447'
+    lqso = LensedQSO(galaxy)
     #mags_to_fluxes(lqso)
     #ned_table_to_sed(lqso,'ned_galex_wise_2mass', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
-    #lqso.plot_spectrum(loglog=True)
+    lqso.plot_spectrum(loglog=True)
+    #tophat(0.5,0.1, 'VLA.txt',True)
     
     plt.show()
---
+++ src/tophat.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 10:35:23 2022
+
+@author: Femke
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def tophat(central,bandwidth,filtername,freq_Ghz=False,length=3000):
+    """
+    This makes a tophat-like profile function for the radio wavelengths, 
+    with transmission =1 within the bandwidth and 0 outside it.
+    """
+        
+    #wavelengths in angstrom or hertz
+    x_array = np.linspace (float(central) - float(bandwidth), float(central) + float(bandwidth), length)
+    
+    #corresponding transmission
+    transmission_array = np.zeros (length)
+    transmission_array[(x_array >= (float(central) - (float(bandwidth) /2)))\
+                * (x_array <= (float(central) + (float(bandwidth) /2)))] = 1
+                       
+    #If given in Ghz, transform to Angstrom
+    if freq_Ghz:
+        wavelength_array = 2.99e8 * (10 ** 10) / ((x_array)*(10**9) )
+        x_array = wavelength_array 
+        
+    plt.plot(x_array, transmission_array)
+    plt.xlabel('wavelength in Angstrom')
+    plt.ylabel('transmission')
+    
+    #writing to the file
+    save_string = ''
+    for i in range(3000):
+        save_string += f'{x_array[i]} '
+        save_string += f'{transmission_array[i]}\n'
+        
+    file = open(os.path.join('data','Filterprofiles','TXT',filtername), "w")
+    file.write(save_string)
+    file.close()
