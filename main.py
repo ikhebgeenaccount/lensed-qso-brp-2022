d3c64fd1edf0b00c4ab5d03e7debe89f03d35336
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-10 12:43:05 +0100

    percentages

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from src.mags_to_fluxes import mags_to_fluxes
 from src.ned_to_sed import ned_table_to_sed
 from src.xml_to_txt import xml_to_txt
 from src.tophat import tophat
+from src.percent_to_fraction import percent_to_fraction
 
 if __name__ == '__main__':
-    #xml_to_txt('HST.ACS_HRC.F330W.xml', 'HST.ACS_HRC.F330W.txt')
+    #filterprofiles
+    #xml_to_txt('WIYN.B_HARRIS.xml', 'WIYN.B_HARRIS.txt')
+    #tophat(15,6, 'VLA_15.txt',True)
+    #percent_to_fraction('WIYN.U_HARRIS.txt','WIYN.U_HARRIS_fraction.txt')
     
-    galaxy = 'B1152+200'
-    lqso = LensedQSO(galaxy)
+    #photometry
+    #galaxy = 'B1152+200'
+    #lqso = LensedQSO(galaxy)
     #mags_to_fluxes(lqso)
     #ned_table_to_sed(lqso,'ned_galex_wise_2mass', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
-    lqso.plot_spectrum(loglog=True, component='_A')
-    tophat(15,6, 'VLA_15.txt',True)
+    #lqso.plot_spectrum(loglog=True)
+    
     
     plt.show()
