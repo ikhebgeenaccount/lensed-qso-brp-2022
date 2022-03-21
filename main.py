2a03df7bb1df6822a1678f38e2a60d94ff331767
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-21 09:19:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ from src.percent_to_fraction import percent_to_fraction
 if __name__ == '__main__':
     #filterprofiles
     #xml_to_txt('WIYN.B_HARRIS.xml', 'WIYN.B_HARRIS.txt')
-    tophat(144e-3,48e-3, 'LOFAR.txt',True)
+    #tophat(144e-3,48e-3, 'LOFAR.txt',True)
     #percent_to_fraction('WIYN.U_HARRIS.txt','WIYN.U_HARRIS_fraction.txt')
     
     #photometry
-    #galaxy = 'J1633+3134'
-    #lqso = LensedQSO(galaxy)
+    galaxy = 'J1633+3134'
+    lqso = LensedQSO(galaxy)
     #mags_to_fluxes(lqso)
     #ned_table_to_sed(lqso,'ned_galex_wise_2mass', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
     #lqso.plot_spectrum(loglog=True)
     
    # model_subtraction(lqso)
-    
+    lqso.sed['telescope']=0
     
     
     plt.show()
