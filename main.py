438ffcda87183f23312b5b49d704f164ab9b0904
femkeballieux <97540397+femkeballieux@users.noreply.github.com>
2022-03-24 11:12:51 +0100

    merge

--- main.py
+++ main.py
@@ -12,25 +12,29 @@ from src.AGN_input import AGN_input
 
 if __name__ == '__main__':
     #filterprofiles
-    xml_to_txt('VLT_CONICA_H.xml', 'VLT_CONICA_H.txt')
+    #xml_to_txt('VLT_CONICA_H.xml', 'VLT_CONICA_H.txt')
     #tophat(230.609583,0.560, 'IRAM_1.3mm.txt',freq_Ghz=True, energy_Kev=False)
     #percent_to_fraction('WIYN.U_HARRIS.txt','WIYN.U_HARRIS_fraction.txt')
     
     #photometry
-    #galaxy = 'B1608+656'
-    #lqso = LensedQSO(galaxy)
+    galaxy = 'J0806+2006'
+    lqso = LensedQSO(galaxy)
     #mags_to_fluxes(lqso)
     #ned_table_to_sed(lqso,'ned_galex_wise_2mass', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
     #lqso.plot_spectrum(loglog=True)
 
     #model subtraction
-    #model_subtraction(lqso)
+    model_subtraction(lqso)
     
-    
+    #making the settings for agn fitter
     #AGN_input('J0806+2006')
     
     
     
+    
+    
+    
+    #running all galaxies
     GALAXIES = ['J0806+2006', 'J0924+0219', 'B1152+200', 'J1330+1810', 'J1455+1447', 'J1524+4409', 'B1600+434', 'B1608+656', 'J1633+3134', 'J1650+4251']
     PLOTS_SAVE = 'plots'
 
