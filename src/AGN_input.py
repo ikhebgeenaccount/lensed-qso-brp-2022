147eae3afbe3fd48755993c7997e0f63d47cc72c
ikhebgeenaccount <lars.abbo@gmail.com>
2022-03-24 13:16:38 +0100

    More fixes

--- src/AGN_input.py
+++ src/AGN_input.py
@@ -17,7 +17,8 @@ def AGN_input_1():
 
         tel=FILTER_PROPERTIES.telescope.values[i]
         TEL=tel.upper()
-        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime')
+        tel = tel.replace(' ', 'space')
+        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
         file=FILTER_PROPERTIES.file.values[i]
 
         if pd.isnull(file):
@@ -33,29 +34,30 @@ def AGN_input_2():
 
         tel=FILTER_PROPERTIES.telescope.values[i]
         TEL=tel.upper()
-        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime')
+        tel = tel.replace(' ', 'space')
+        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
         file=FILTER_PROPERTIES.file.values[i]
 
         if pd.isnull(file):
             continue
 
-        print(f"    if filters[_{tel}_{fil}]:")
-        print(f"        files.append(_{tel}_{fil}_file)")
-        print(f"        lambdas.append(_{tel}_{fil}_lambda)")
-        print(f"        factors.append(_{tel}_{fil}_factor)")
+        print(f"        if filters['_{tel}_{fil}']:")
+        print(f"            files.append(_{tel}_{fil}_file)")
+        print(f"            lambdas.append(_{tel}_{fil}_lambda)")
+        print(f"            factors.append(_{tel}_{fil}_factor)")
         print('')
     print('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
 
 def AGN_input_3(galaxy=None):
     lqso = LensedQSO(galaxy)
-    print(lqso.sed[['telescope', 'filter']].values)
     for i, row in FILTER_PROPERTIES.iterrows():
 
         #Iterate over each row to get the dictionary settings
 
         tel=FILTER_PROPERTIES.telescope.values[i]
         TEL=tel.upper()
-        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime')
+        tel = tel.replace(' ', 'space')
+        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
         file=FILTER_PROPERTIES.file.values[i]
 
 
@@ -71,7 +73,7 @@ def AGN_input_3(galaxy=None):
          #Als je wel een glaxy geeft zet je hem op true wanneer hij er in zit, false als niet
         else:
             # if [tel,fil] in lqso.sed[['telescope', 'filter']].values:
-            if lqso.filter_sed().loc[(lqso.filter_sed()['telescope'] == tel) * (lqso.filter_sed()['filter'] == fil.replace("prime", "'"))].shape[0] > 0:
+            if lqso.filter_sed().loc[(lqso.filter_sed()['telescope'] == tel) * (lqso.filter_sed()['filter'] == fil.replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot'))].shape[0] > 0:
                 print(f"    filters['_{tel}_{fil}']=True")
             else:
                 print(f"    filters['_{tel}_{fil}']=False")
