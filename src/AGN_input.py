19ae16256bea45a6d14f5b22605662abfcd6b3b1
unknown <lars.abbo@gmail.com>
2022-03-24 17:28:50 +0100

    Bug fixes

--- src/AGN_input.py
+++ src/AGN_input.py
@@ -7,36 +7,37 @@ import os
 from src.lensed_qso import LensedQSO
 from src.filters import get_filename
 
-
 FILTER_PROPS_PATH = os.path.join('data', 'filter.csv')
 FILTER_PROPERTIES = pd.read_csv(FILTER_PROPS_PATH)
 
-def AGN_input_1():
 
+def AGN_input_1():
     for i, row in FILTER_PROPERTIES.iterrows():
 
-        tel=FILTER_PROPERTIES.telescope.values[i]
-        TEL=tel.upper()
+        tel = FILTER_PROPERTIES.telescope.values[i]
+        TEL = tel.upper()
         tel = tel.replace(' ', 'space')
-        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
-        file=FILTER_PROPERTIES.file.values[i]
+        fil = FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
+        file = FILTER_PROPERTIES.file.values[i]
 
         if pd.isnull(file):
             continue
-        #print(tel, fil, file)
+        # print(tel, fil, file)
         print(f"    _{tel}_{fil}_file = path + 'models/FILTERS/{TEL}/{file}'")
-        print(f"    _{tel}_{fil}_lambda,_{tel}_{fil}_factor = np.loadtxt(_{tel}_{fil}_file, usecols=(0,1), unpack = True)")
+        print(
+            f"    _{tel}_{fil}_lambda,_{tel}_{fil}_factor = np.loadtxt(_{tel}_{fil}_file, usecols=(0,1), unpack = True)")
         print("")
     print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHH')
+
 
 def AGN_input_2():
     for i, row in FILTER_PROPERTIES.iterrows():
 
-        tel=FILTER_PROPERTIES.telescope.values[i]
-        TEL=tel.upper()
+        tel = FILTER_PROPERTIES.telescope.values[i]
+        TEL = tel.upper()
         tel = tel.replace(' ', 'space')
-        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
-        file=FILTER_PROPERTIES.file.values[i]
+        fil = FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
+        file = FILTER_PROPERTIES.file.values[i]
 
         if pd.isnull(file):
             continue
@@ -48,39 +49,31 @@ def AGN_input_2():
         print('')
     print('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
 
+
 def AGN_input_3(galaxy=None):
     lqso = LensedQSO(galaxy)
+
+    # Iterate over each row to get the dictionary settings
     for i, row in FILTER_PROPERTIES.iterrows():
 
-        #Iterate over each row to get the dictionary settings
+        tel = FILTER_PROPERTIES.telescope.values[i]
+        fil = FILTER_PROPERTIES.filtername.values[i]
+        file = FILTER_PROPERTIES.file.values[i]
 
-        tel=FILTER_PROPERTIES.telescope.values[i]
-        TEL=tel.upper()
-        tel = tel.replace(' ', 'space')
-        fil=FILTER_PROPERTIES.filtername.values[i].replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
-        file=FILTER_PROPERTIES.file.values[i]
-
-
-        #als je geen galaxy geeft zet ze allemaal op nul
-        if pd.isnull(galaxy):
+        # als je geen galaxy geeft zet ze allemaal op nul
+        if galaxy is None:
             if pd.isnull(file):
                 continue
 
             print(f'    filters[_{tel}_{fil}]=True')
 
-
-
-         #Als je wel een glaxy geeft zet je hem op true wanneer hij er in zit, false als niet
+        # Als je wel een glaxy geeft zet je hem op true wanneer hij er in zit, false als niet
         else:
             # if [tel,fil] in lqso.sed[['telescope', 'filter']].values:
-            if lqso.filter_sed().loc[(lqso.filter_sed()['telescope'] == tel) * (lqso.filter_sed()['filter'] == fil.replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot'))].shape[0] > 0:
-                print(f"    filters['_{tel}_{fil}']=True")
+            if lqso.filter_sed(component='_sub').loc[(lqso.filter_sed(component='_sub')['telescope'] == tel) * (
+                    lqso.filter_sed(component='_sub')['filter'] == fil)].shape[0] > 0:
+                f = fil.replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
+                print(f"    filters['_{tel.replace(' ', 'space')}_{f}']=True")
             else:
-                print(f"    filters['_{tel}_{fil}']=False")
-
-
-
-
-
-
-
+                f = fil.replace("'", 'prime').replace('-', 'dash').replace('/', 'slash').replace('.', 'dot')
+                print(f"    filters['_{tel.replace(' ', 'space')}_{f}']=False")
