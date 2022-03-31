920270467b882cfa31fccfc37272f047c284af38
ikhebgeenaccount <lars.abbo@gmail.com>
2022-03-31 10:43:25 +0200

    Fixed speagle plot to be based on age of universe instead of age of galaxy

--- src/speagle.py
+++ src/speagle.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 30 11:15:07 2022
+from astropy.cosmology import LambdaCDM
 
-@author: abbo
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+LCDM = LambdaCDM(H0=70, Om0=0.3, Ode0=0.7)  # Cosmological constants as Speagle uses them
 
 log_m_stars = np.linspace(8, 14, num=10000)
 
@@ -43,23 +39,38 @@ def speagle_gms(log_m_star, t, log_m_star_err=None, t_err=None):
 
 def plot_lqso_in_speagle(lqso, ax=None):
     # Get age
-    t = lqso.agn_fitter_output()['age'].iloc[2]
-    t_pe = lqso.agn_fitter_output()['age'].iloc[3] - t
-    t_me = t - lqso.agn_fitter_output()['age'].iloc[1]
+    #t = np.power(10., lqso.agn_fitter_output()['age'].iloc[2])
+    #t_pe = (lqso.agn_fitter_output()['age'].iloc[3] - t) * np.log(10.) * t
+    #t_me = (t - lqso.agn_fitter_output()['age'].iloc[1]) * np.log(10.) * t
+    #t = t * np.power(10., -9)
+    #t_pe = t_pe * np.power(10., -9)
+    #t_me = t_me * np.power(10., -9)
+
+    z = lqso.props.z_qso.values[0]
+    t = LCDM.age(z).value
+
     # Star formation rate
-    sfr = lqso.agn_fitter_output()['SFR_IR'].iloc[2]
-    sfr_pe = lqso.agn_fitter_output()['SFR_IR'].iloc[3] - sfr
-    sfr_me = sfr - lqso.agn_fitter_output()['SFR_IR'].iloc[1]
+    sfr_ir = lqso.agn_fitter_output()['SFR_IR'].iloc[2]
+    sfr_ir_pe = lqso.agn_fitter_output()['SFR_IR'].iloc[3] - sfr_ir
+    sfr_ir_me = sfr_ir - lqso.agn_fitter_output()['SFR_IR'].iloc[1]
+
+    sfr_opt = lqso.agn_fitter_output()['SFR_opt'].iloc[2]
+    sfr_opt_pe = lqso.agn_fitter_output()['SFR_opt'].iloc[3] - sfr_opt
+    sfr_opt_me = sfr_opt - lqso.agn_fitter_output()['SFR_opt'].iloc[1]
+
     # logM_star
     log_m_star = lqso.agn_fitter_output()['logMstar'].iloc[2]
     log_m_star_pe = lqso.agn_fitter_output()['logMstar'].iloc[3] - log_m_star
     log_m_star_me = log_m_star - lqso.agn_fitter_output()['logMstar'].iloc[1]
 
+    # Magnification
+    mu = 10
+
     if ax is None:
         # These things are only done if no ax is given
         fig, ax = plt.subplots()
 
-        sp_ms, sp_ms_err = speagle_gms(log_m_stars, t, t_err=t_me)
+        sp_ms, sp_ms_err = speagle_gms(log_m_stars, t)
 
         ax.fill_between(log_m_stars, sp_ms - sp_ms_err, sp_ms + sp_ms_err, alpha=.6, label='Speagle+2014 GMS')
 
@@ -68,10 +79,15 @@ def plot_lqso_in_speagle(lqso, ax=None):
 
     # Format errors properly
     xerr = np.array([log_m_star_me, log_m_star_pe]).reshape((2, 1))
-    yerr = np.array([sfr_me, sfr_pe]).reshape((2, 1))
-    yerr = yerr / (sfr * np.log(10.))  # Calculate error in log SFR from SFR
+    yerr_ir = np.array([sfr_ir_me, sfr_ir_pe]).reshape((2, 1))
+    yerr_ir = yerr_ir / (sfr_ir * np.log(10.))  # Calculate error in log SFR from SFR
+
+    xerr = np.array([log_m_star_me, log_m_star_pe]).reshape((2, 1))
+    yerr_opt = np.array([sfr_opt_me, sfr_opt_pe]).reshape((2, 1))
+    yerr_opt = yerr_opt / (sfr_opt * np.log(10.))  # Calculate error in log SFR from SFR
 
     # Plot galaxy
-    ax.errorbar(log_m_star, np.log10(sfr), xerr=xerr, yerr=yerr, label=lqso.name, fmt='o')
+    ax.errorbar(log_m_star - np.log10(mu), np.log10(sfr_ir/mu), xerr=xerr, yerr=yerr_ir, label=lqso.name + ' SFR_IR', fmt='o')
+    ax.errorbar(log_m_star - np.log10(mu), np.log10(sfr_opt/mu), xerr=xerr, yerr=yerr_opt, label=lqso.name + ' SFR_opt', fmt='o')
 
     ax.legend()
