f82b99e2edf4c4e2e1dd64a81dacbca8d0644321
Femke Ballieux <ballieux@student76.strw.leidenuniv.nl>
2022-04-07 10:58:50 +0200

    changed main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from src.filters import populate_filter_profile_path_column
 
 if __name__ == '__main__':
     #photometry
-    galaxy = 'J1524+4409' 
+    galaxy = 'B1608+656' 
     lqso = LensedQSO(galaxy)
     #ned_table_to_sed(lqso,'ned_galex_wise_2mass', allowed_sources=['Chandra', 'WISE', '2MASS', 'Galex'])
     lqso.plot_spectrum(loglog=True, component='_sub')
@@ -26,8 +26,8 @@ if __name__ == '__main__':
     #model_subtraction(lqso)
     
     #AGN input
-    print(lqso.sed_to_agn_fitter())
-    #print(lqso.agn_settings())
+    #print(lqso.sed_to_agn_fitter(rX=False))
+    print(lqso.agn_settings(rX=False))
     
     #speagle
     #plot_lqso_in_speagle(lqso)
