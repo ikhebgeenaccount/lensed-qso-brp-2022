ee9bfe90abca7a5d523062d2572cfe37749d516d
ikhebgeenaccount <lars.abbo@gmail.com>
2022-04-25 15:34:46 +0200

    TODO added for uplims in lqso.plot_spectrum

--- src/lensed_qso.py
+++ src/lensed_qso.py
@@ -235,7 +235,7 @@ class LensedQSO:
 
             if len(sel_upper_limit) > 0:
                 le_2, _, _ = ax.errorbar(sel_upper_limit.wavelength, sel_upper_limit[data_type], sel_upper_limit[data_err],
-                                         fmt='v', label=l, color=le_1.get_color(), **kwargs)
+                                         fmt='v', label=l, color=le_1.get_color())#, uplims=True, capsize=0, **kwargs) TODO: uplims
                 legend_list.append((le_1, le_2))
             else:
                 legend_list.append(le_1)
