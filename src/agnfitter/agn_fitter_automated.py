b559aace0849b30bee529c7f5405e1e8a81c2249
unknown <lars.abbo@gmail.com>
2022-06-21 15:19:54 +0200

    fixed paths

--- src/agnfitter/agn_fitter_automated.py
+++ src/agnfitter/agn_fitter_automated.py
@@ -6,15 +6,12 @@ import os
 RUN_TEN_MODE = 'python ../AGNfitter/RUN_AGNfitter_multi.py ../AGNfitter/example/SETTINGS_AGNfitter_{name}.py --overwrite'
 RUN_SINGLE_MODE = RUN_TEN_MODE + ' -n 0'
 
-AGN_FITTER_PATH = os.path.join(os.pardir, '')
-AGN_FITTER_RX_PATH = os.path.join(os.pardir, 'AGNfitter-rX_v0.1', '')
+AGN_FITTER_PATH = App.config().get(section='AGNFITTER', option='agnfitter_path')
+# AGN_FITTER_RX_PATH = os.path.join(os.pardir, 'AGNfitter-rX_v0.1', '')
 
 
-def run_agn_fitter(galaxies, rX=False, run_times=1, settings=None, component='_sub_demag'):
-    if rX:
-        path = AGN_FITTER_RX_PATH
-    else:
-        path = AGN_FITTER_PATH
+def run_agn_fitter(galaxies, rX=False, run_times=1, component='_sub_demag'):
+    path = AGN_FITTER_PATH
 
     for g in galaxies:
         lqso = LensedQSO(g)
@@ -23,12 +20,12 @@ def run_agn_fitter(galaxies, rX=False, run_times=1, settings=None, component='_s
         print(f'Updating catalog for {lqso.name}')
         cat, l = lqso.sed_to_agn_fitter(rX=rX, component=component, run_times=run_times)
 
-        with open(os.path.join(path, App.config().get(section='GENERAL', option='data_dir'), f'{lqso.name}.txt'), 'w') as cf:
+        with open(os.path.join(path, 'data', f'{lqso.name}.txt'), 'w') as cf:
             cf.write(cat)
 
         # Update settings
         print(f'Updating settings for {lqso.name}')
-        settings = lqso.agn_fitter_settings(rX=rX, settings=settings)
+        settings = lqso.agn_fitter_settings(rX=rX)
         with open(os.path.join(path, 'example', f'SETTINGS_AGNfitter_{lqso.name}.py'), 'w') as sf:
             sf.write(settings)
 
